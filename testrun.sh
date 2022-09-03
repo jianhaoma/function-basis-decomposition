47fb03f2b5406e3845d7f32c4c2ad0cfe383a1a3
LingjunGuo2333 <75191980+LingjunGuo2333@users.noreply.github.com>
2022-09-03 16:00:48 -0400

    .sh file for train.py

---
+++ testrun.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name train
+#SBATCH --nodes=1
+#SBATCH --time=03:59:59
+#SBATCH --account=fattahi0
+#SBATCH --mail-user=lingjunguo2333@gmail.com
+#SBATCH --mail-type=FAIL
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem-per-gpu=20G
+source /home/glingjun/Summer_proj/buch-expris
+srun -l python3 train.py --no-call_wandb --model='fc-elu' --init_scale=1 --data='Cifar100' --loss_fn='mse_loss' --optimizer='sgd' --batch_size=24 --num_epoch=1 --lr_setting 1e-1 5 1e-2 --decay_rate=0.33 --decay_stepsize=50 --seed=0 --num_run=0 --no-gradient 
+echo "Finished!"
