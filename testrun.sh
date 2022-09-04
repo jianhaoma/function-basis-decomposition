51c25536cf26e25d4b1eae3f91813ab9a7ef7b95
LingjunGuo2333 <75191980+LingjunGuo2333@users.noreply.github.com>
2022-09-04 12:52:29 -0400

    Update testrun.sh

--- testrun.sh
+++ testrun.sh
@@ -9,5 +9,5 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem-per-gpu=20G
 source file_path
-srun -l python3 train.py --no-call_wandb --model='fc-elu' --init_scale=1 --data='Cifar100' --loss_fn='mse_loss' --optimizer='sgd' --batch_size=24 --num_epoch=1 --lr_setting 1e-1 5 1e-2 --decay_rate=0.33 --decay_stepsize=50 --seed=0 --num_run=0 --no-gradient 
+srun -l python3 train.py --call_wandb --model='fc-elu' --init_scale=1 --data='Cifar100' --loss_fn='mse_loss' --optimizer='sgd' --batch_size=24 --num_epoch=1 --lr_setting 1e-1 5 1e-2 --decay_rate=0.33 --decay_stepsize=50 --seed=0 --num_run=0 --no-gradient 
 echo "Finished!"
